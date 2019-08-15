3c2a4457cba22f695b6f8981d0d66f123cd25972
Paul Kim <paulbkim94@gmail.com>
2019-08-15 15:52:15 -0400

    Fixed github link for airbnb

--- run.py
+++ run.py
@@ -209,5 +209,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
 if __name__ =='__main':
-        app.run()
+    app.run()
